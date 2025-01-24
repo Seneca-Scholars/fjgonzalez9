d254ed16889eaee590c5366f9f99eacaba73015a
Fernando Gonzalez <fjgonzalez9@Fernandos-MacBook-Pro.local>
2025-01-23 17:05:43 -0800

    New Updates

---
+++ ReactContext-Hooks/my-app/src/context/ThemeProvider.jsx
@@ -0,0 +1,36 @@
+import React, { createContext, useState, useContext } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  const themeStyles = {
+    light: {
+      backgroundColor: "#ffffff",
+      color: "#000000",
+    },
+    dark: {
+      backgroundColor: "#000000",
+      color: "#ffffff",
+    },
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme, themeStyles }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+};
